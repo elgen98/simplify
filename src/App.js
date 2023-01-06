f4f114bf06760e968b015425386d37b61d5f5e4b
Elliot Blomqvist <elliot.blomqvist@yahoo.com>
2023-01-06 14:06:40 +0100

    access token aquired and node package installed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import Login from './components/Login';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Login from "./components/Login";
+import { getTokenFromUrl } from "./spotify";
 
 function App() {
-  return (
-    <Login />
-  );
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+
+    console.log("token", token);
+  }, []);
+
+  return <div className="app">{token ? <h1>Logged in</h1> : <Login />}</div>;
 }
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react'
-
-const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=0aed6757686b4616b75fafaa85b596f5&response_type=code&redirect_uri=http://localhost:3000&scope=user-read-email%20user-read-private%20playlist-read-private%20playlist-read-collaborative%20playlist-modify-private%20playlist-modify-public'
-
-export default class Login extends Component {
-  render() {
-    return (
-      <div>
-        <a class="" href={AUTH_URL}>Login With Spotify</a>
-      </div>
-    )
-  }
+import React from "react";
+import { loginUrl } from "../spotify";
+function Login() {
+  return (
+    <div>
+      <a href={loginUrl}>Login to Spotify</a>
+    </div>
+  );
 }
+
+export default Login;
