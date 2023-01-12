1f25b7633ee19b29cdc6da35f8fa69e60dd86ff4
Elliot Blomqvist <elliot.blomqvist@yahoo.com>
2023-01-12 09:27:52 +0100

    get playlists in edit for move function

--- src/components/EditMode.js
+++ src/components/EditMode.js
@@ -1,9 +1,26 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function EditMode(props) {
   const playlist = props.playlist;
   const [selectedTracks, setSelectedTracks] = useState([]);
+  const [playlists, setPlaylists] = useState([]);
+
+  useEffect(() => {
+    Promise.all([spotify.getMe(), spotify.getUserPlaylists({ limit: 50 })])
+      .then((values) => {
+        let userId = values[0].id;
+        setPlaylists(
+          values[1].items.filter(function (item) {
+            return item.owner.id === userId;
+          })
+        );
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
   function toggleChecked(e) {
     const { value, checked } = e.target;
@@ -33,6 +50,7 @@ function EditMode(props) {
           <button onClick={() => props.removeTracks(selectedTracks)}>
             Delete
           </button>
+          <buttton>Move to another playlist</buttton>
         </div>
       )}
       <div className="flex flex-col gap-2 w-3/4">{playlistHtml}</div>
