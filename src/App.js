6a651281b6d5e88b60d891d76c7455d49e72f86e
elgen98 <elliot.blomqvist@yahoo.com>
2023-01-13 02:59:47 +0100

    moved Simplify to App

--- src/App.js
+++ src/App.js
@@ -4,26 +4,45 @@ import Login from "./components/Login";
 import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
 import PlaylistSelection from "./components/PlaylistSelection";
-import Simplify from "./components/Simplify";
+import PlaylistManager from "./components/PlaylistManager";
 
 const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState("");
+    const [token, setToken] = useState("");
+    const [playlistId, setPlaylistId] = useState("");
 
-  useEffect(() => {
-    //GET token from url and resetting hash
-    const hash = getTokenFromUrl();
-    window.location.hash = "";
-    const _token = hash.access_token;
+    useEffect(() => {
+        //GET token from url and resetting hash
+        const hash = getTokenFromUrl();
+        window.location.hash = "";
+        const _token = hash.access_token;
 
-    if (_token) {
-      setToken(_token);
-      spotify.setAccessToken(_token);
+        if (_token) {
+            setToken(_token);
+            spotify.setAccessToken(_token);
+        }
+    }, []);
+
+    function selectPlaylist(playlistId) {
+        setPlaylistId(playlistId);
     }
-  }, []);
 
-  return <div className=" h-screen">{token ? <Simplify /> : <Login />}</div>;
+    let main = (
+        <div>
+            <h2 className="text-2xl font-semibold ">Your Playlists</h2>
+            <PlaylistSelection liftId={selectPlaylist} />
+        </div>
+    );
+    if (playlistId) {
+        main = <PlaylistManager id={playlistId} />;
+    }
+
+    return (
+        <main className="flex flex-col items-center gap-4 w-screen h-screen">
+            {token ? main : <Login />}
+        </main>
+    );
 }
 
 export default App;
