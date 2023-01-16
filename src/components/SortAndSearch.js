cd931d3e1d7ed4876b3d49c6a9ba73c1a3d55d78
elgen98 <elliot.blomqvist@yahoo.com>
2023-01-16 16:17:57 +0100

    alphabetically sorting working

--- src/components/SortAndSearch.js
+++ src/components/SortAndSearch.js
@@ -3,23 +3,50 @@ import React from "react";
 function SortAndSearch(props) {
     let playlist = props.playlist;
 
-    function dateComparison(a, b) {
-        const date1 = new Date(a.added_at);
-        const date2 = new Date(b.added_at);
-
-        return date2 - date1;
+    function sorting(e) {
+        if (e.target.value === "oldest") {
+            playlist.sort(function (a, b) {
+                return new Date(a.added_at) - new Date(b.added_at);
+            });
+        }
+        if (e.target.value === "recent") {
+            playlist.sort(function (a, b) {
+                return new Date(b.added_at) - new Date(a.added_at);
+            });
+        }
+        if (e.target.value === "A-Ö") {
+            playlist.sort(function (a, b) {
+                let nameA = a.track.name.toUpperCase();
+                let nameB = b.track.name.toUpperCase();
+                if (nameA < nameB) {
+                    return -1;
+                }
+                if (nameA > nameB) {
+                    return 1;
+                }
+                return 0;
+            });
+        }
+        console.log(playlist);
+        props.liftPlaylist(playlist);
     }
+
     return (
         <div>
-            <button
-                onClick={() => {
-                    playlist.sort(dateComparison);
-                    console.log(playlist);
-                    props.liftPlaylist(playlist);
-                }}
-            ></button>
+            <>
+                <div>
+                    <label for="sort">Sort playlist</label>
+                    <select name="sort" id="sort" onChange={(e) => sorting(e)}>
+                        <option value="oldest">Oldest</option>
+                        <option value="recent">Recent</option>
+                        <option value="A-Ö">A-Ö</option>
+                    </select>
+                </div>
+            </>
         </div>
     );
+    {
+    }
 }
 
 export default SortAndSearch;
