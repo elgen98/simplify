4cd5226f56a117b19632bc2cc1cd43882a79bfc4
elgen98 <elliot.blomqvist@yahoo.com>
2023-01-17 14:47:49 +0100

    styled login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { loginUrl } from "../spotify";
 function Login() {
-  return (
-    <main className=" flex flex-col justify-center items-center gap-40 h-full">
-      <h1 className=" text-5xl text-green-600">Simplify</h1>
-      <a
-        href={loginUrl}
-        className="border rounded-full inline-block w-60 h-14 text-center pt-4 bg-green-600"
-      >
-        Login with Spotify
-      </a>
-    </main>
-  );
+    return (
+        <main className=" flex flex-col justify-center items-center gap-40 min-h-screen bg-nice-gray">
+            <h1 className=" text-5xl text-nice-yellow font-outline-05">
+                Simplify
+            </h1>
+            <a
+                href={loginUrl}
+                className="rounded-full inline-block w-60 h-14 text-center pt-4 bg-gray-600 text-nice-yellow"
+            >
+                Login with Spotify
+            </a>
+        </main>
+    );
 }
 
 export default Login;
